55848eea5166a2092fdf9dce5b64a9592d46f574
JemiArtistaa <mmislam020409@gmail.com>
2023-01-28 00:52:59 +0600

    Update e2e.js

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -15,6 +15,7 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands'
+import allureWriter from "@shelex/cypress-allure-plugin/writer"
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
